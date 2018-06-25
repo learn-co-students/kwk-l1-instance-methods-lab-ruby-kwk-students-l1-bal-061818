c1a0332a8f24015ccb090de8c43cd138c0f849d7
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-25 13:02:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # dog.rb
+
+class Dog 
+  
+  def bark (bark)
+    @bark = bark 
+  end 
+
+  def bark 
+    puts "Woof!"
+  end 
+  
+  
+  def sit (sit)
+    @sit = sit 
+  end 
+  
+  def sit 
+    puts "The Dog is sitting"
+  end 
+  
+  
+  
+end 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,20 @@
 # person.rb
+
+class Person 
+  def talk (talk)
+    @talk = talk 
+  end 
+  
+  def talk 
+    puts "Hello World!"
+  end 
+  
+  def walk (walk)
+    @walk = walk 
+  end 
+  
+  def walk 
+    puts "The Person is walking"
+  end 
+  
+end 
